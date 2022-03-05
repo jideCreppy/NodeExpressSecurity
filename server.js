dcbafc24cba8a015b1109ef2bf5708935b65cd38
Jide Creppy <jide.creppy@addpeople.co.uk>
2022-03-05 03:00:09 +0000

    Code refactor

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ mongoose.connect('mongodb+srv://root:root@cluster0.ezyie.mongodb.net/crm?retryWr
 // JWT setup
 const tokenSecret = 'RESTFULLAPIs';
 app.use((req, res, next) => {
-
     if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] == 'JWT') {
         jsonwebtoken.verify(req.headers.authorization.split(' ')[1], tokenSecret, (err, decode) => {
             if (err) req.user == undefined;
@@ -30,7 +29,6 @@ app.use((req, res, next) => {
        req.user = undefined;
        next();
     }
-
 });
 
 // Routes
